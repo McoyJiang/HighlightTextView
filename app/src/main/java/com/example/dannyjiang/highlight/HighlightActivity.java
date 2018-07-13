4ea7d2520b01bc53832cf80d43af59cc3478ee61
Danny.Jiang <ext.danny.jiang@ef.com>
2018-07-13 22:31:49 +0800

    add IMAGES

--- app/src/main/java/com/example/dannyjiang/highlight/HighlightActivity.java
+++ app/src/main/java/com/example/dannyjiang/highlight/HighlightActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class HighlightActivity extends AppCompatActivity {
 
+    private HighlightTextView highlightTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highlight);
+
+        highlightTextView = findViewById(R.id.highlight);
+        //highlightTextView.highlight(true);
     }
 }
