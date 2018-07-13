a9bc532a5eccbc695ec1a8f0bf61edf5d9798b03
Danny.Jiang <ext.danny.jiang@ef.com>
2018-07-13 23:47:21 +0800

    add final code

--- app/src/main/java/com/example/dannyjiang/highlight/HighlightActivity.java
+++ app/src/main/java/com/example/dannyjiang/highlight/HighlightActivity.java
@@ -3,6 +3,7 @@ package com.example.dannyjiang.highlight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class HighlightActivity extends AppCompatActivity {
 
@@ -15,6 +16,12 @@ public class HighlightActivity extends AppCompatActivity {
 
         highlightTextView = findViewById(R.id.highlight);
         highlightTextView.highlight(false);
+        highlightTextView.setOnWordClickListener(new HighlightTextView.OnWordClickListener() {
+            @Override
+            public void onWordClicked(String text) {
+                Toast.makeText(HighlightActivity.this, "点击了：" + text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void highlightText(View view) {
